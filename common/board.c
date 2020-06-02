9e8e35e0f6fae1facd1ea8b271a2c2f9c346adf1
Jehan Diaz 22 <buenosdiaz@flume.cs.dartmouth.edu>
2020-06-02 13:18:02 -0400

    gitignore updated

--- common/board.c
+++ common/board.c
@@ -9,13 +9,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-typedef struct sudoku {
-  int **board;   // Points to a 2D array of values holding the board
-  int dimension; // Tells parsers how long each row and column are
-} sudoku_t;
-
-
 /************ generateBoard ************/
 /*
  * Generates a blank 9x9 sudoku board filled with 0s
