4d71f08b4ab32b0070aba2bc1808e99a41f9dfb7
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-06-02 18:31:14 -0400

    Initial working loading and styling

--- gui/src/components/app.js
+++ gui/src/components/app.js
@@ -6,6 +6,8 @@ import { createBoard, solveBoard } from '../actions/boardActions';
 import { createErrorSelector } from '../actions/errorActions';
 import { createLoadingSelector } from '../actions/loadingActions';
 
+import LoadingIcon from './loadingIcon';
+
 import './app.scss';
 
 class App extends React.Component {
@@ -21,31 +23,50 @@ class App extends React.Component {
 
   isHighlighted(ri, ci) {
     if (ri === this.state.posRow && ci === this.state.posCol) return false; // Don't highlight selected square
+
+    // const rg = Math.floor(ri / 3);
+    // const cg = Math.floor(ri / 3);
+
+    // const rg = Math.trunc(ri / 3) * 3;
+    // const cg = Math.trunc(ri / 3) * 3;
+
+    // if (
+    //   (rg <= this.state.posRow && this.state.posRow < rg + 3)
+    //   && (cg <= this.state.posCol && this.state.posCol < cg + 3)
+    // ) { console.log(this.state.posRow, this.state.posCol, rg, cg); return true; }
+
     if (this.state.posRow === ri || this.state.posCol === ci) return true; // In same row or column
+
     return false;
   }
 
   renderBoard() {
     return (
       <div className="app-board-container">
-        {/* {JSON.stringify(this.props.board)} */}
-        {this.props.board.map((r, ri) => {
-          return (
-            <div key={ri} className="app-board-row-container">
-              {r.map((c, ci) => {
-                return (
-                  <div
-                    key={`${ri} ${ci}`}
-                    className={`app-board-cell-container${this.isHighlighted(ri, ci) ? ' highlighted' : ''}`}
-                    onMouseEnter={() => this.setState({ posRow: ri, posCol: ci })}
-                  >
-                    <p>{c}</p>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+        {this.props.board && this.props.dimension
+          ? (
+            <>
+              {this.props.isLoading === true
+                ? <LoadingIcon />
+                : this.props.board.map((r, ri) => {
+                  return (
+                    <div key={ri} className="app-board-row-container">
+                      {r.map((c, ci) => {
+                        return (
+                          <div
+                            key={`${ri} ${ci}`}
+                            className={`app-board-cell-container${this.isHighlighted(ri, ci) ? ' highlighted' : ''}`}
+                            onMouseEnter={() => this.setState({ posRow: ri, posCol: ci })}
+                          >
+                            <p>{c}</p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+            </>
+          ) : <p>Empty Board</p>}
       </div>
     );
   }
@@ -57,11 +78,6 @@ class App extends React.Component {
 
         <button type="button" onClick={() => this.props.createBoard(this.state.difficulty)}>Create Board</button>
         <button type="button" onClick={() => this.props.solveBoard(this.props.board, this.props.dimension)}>Solve Board</button>
-
-        {this.props.board && this.props.dimension
-          ? this.renderBoard()
-          : null}
-
         <input
           type="number"
           min={1}
@@ -69,6 +85,10 @@ class App extends React.Component {
           value={this.state.difficulty}
           onChange={e => this.setState({ difficulty: e.target.value })}
         />
+
+        <div style={{ height: '60px' }} />
+
+        {this.renderBoard()}
 
       </div>
     );
